d0175e8dd2d820ea14a8e7cc0de1e99c2f884822
WLawrence <wesley.lawrence94@gmail.com>
2025-07-09 12:59:43 -0400

    Added a dummy list of 6 beasts for testing purposes

---
+++ Bestiary/src/app/dummy-beasts.ts
@@ -0,0 +1,84 @@
+export const dummyBeasts =  [
+    {
+    
+        species: 'Ironlanders',
+        name: 'Broken',
+        rank: 'Troublesome (3 progress per harm; inflicts 1 harm)',
+        traits: [],
+        features: ['Crazed skin', 'Painted skin', 'Feral screams', 'Scavenged clothing and weapons'],
+        drives: ['Show my power', 'Share my pain'],
+        tactics: ['Spring from hiding', 'ferocious attacks'],
+        desc: 'Another people sailed to the Ironlands from the Old World long before our kin settled here.'
+
+    }, 
+
+     {
+    
+        species: 'Ironlanders',
+        name: 'Hunter',
+        rank: 'Dangerous (2 progress per harm; inflicts 2 harm)',
+        traits: ['Wearing hides and fur to ward away the cold', 'Steely gaze', 'At home in the woodlands'],
+        features: [],
+        drives: ['A clean kill', 'Survive the hunt'],
+        tactics: ['Set traps', 'Keep to the shadows', 'Deadly shot'],
+        desc: 'Hunters face brutal weather, difficult terrain, dangerous animals, and worse.'
+
+    }, 
+
+    {
+    
+        species: 'Animals',
+        name: 'Bear',
+        rank: 'Formidable (1 progress per harm; inflicts 3 harm)',
+        traits: [],
+        features: ['Fearsome teeth and claws', 'Thick hide'],
+        drives: ['Find food','Defend cubs'],
+        tactics: ['Roar', 'Pin down', 'Maul with savage force'],
+        desc: 'Most bears are not aggressive.'
+
+    }, 
+
+    {
+    
+        species: 'Animals',
+        name: 'Boar',
+        rank: 'Dangerous (2 progress per harm; inflicts 2 harm)',
+        traits: [],
+        features: ['Wiry coat', 'Long tusks', 'Vicious'],
+        drives: ['Forage', 'Protect territory', 'Defend sows'],
+        tactics: ['Charge and gore', 'Circle and attack again'],
+        desc: 'In the Old World, wild boars were belligerent and dangerous animals.'
+
+    }, 
+
+     {
+    
+        species: 'Horrors',
+        name: 'Chimera',
+        rank: 'Extreme (2 progress per harm; inflicts 4 harm)',
+        traits: [],
+        features: ['Shambling mass of dead creatures and offal', 'Rotting stench'],
+        drives: ['Insatiable hunger'],
+        tactics: ['Horrifying wail', 'Relentless assault', 'Claw, bite and rend'],
+        desc: 'A chimera is the corrupted form of dead animal flesh given horrible life.'
+
+    }, 
+
+
+    {
+    
+        species: 'Horrors',
+        name: 'Haunt',
+        rank: 'Formidable (1 progress per harm; inflicts 3 harm)',
+        traits: [],
+        features: ['Subtle, unsettling manifestations', 'Appear as they did in life', 'Lay bare the ravages of death', 'Stench of the grave'],
+        drives: ['Torment the living', 'Find rest'],
+        tactics: ['Vanish and reappear', 'Horrifying visage', 'Unleash chaos'],
+        desc: 'Haunts are restless spirits bound to this world by a traumatic or unjust death.'
+
+    }  
+
+
+
+
+]
