99a2fe19c07449bbdceb07e301bd87b2c09feebc
WLawrence <wesley.lawrence94@gmail.com>
2025-07-16 01:57:26 -0400

    [REMOVED] Removed the unnessecary displaycreatures function

---
+++ Bestiary/src/app/bestiary-list-display-component/creature-display-component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { dummyBeasts } from '../dummy-beasts';
+import { speciesList } from '../species-list';
+import { BestiaryEntry } from '../bestiary-entry.model';
+
+@Component({
+  selector: 'app-creature-display-component',
+  imports: [],
+  templateUrl: './creature-display-component.html',
+  styleUrl: './creature-display-component.css'
+})
+export class CreatureDisplayComponent {
+
+  creatureList = dummyBeasts;
+  @Input ({required: true}) creatureNameInput!: string;
+
+}
